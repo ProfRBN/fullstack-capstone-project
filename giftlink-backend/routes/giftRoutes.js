37ecfb33cc2fb8168e3c61623ba24b12cf56c296
ProfRBN <nylfson@yahoo.com.br>
2026-04-07 11:05:06 -0400

    arquivos ajustados

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,49 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+const logger = require('../logger');
 
-router.get('/', async (req, res) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("gifts");
-        const gifts = await collection.find({}).toArray();
-        res.json(gifts);
-    } catch (e) {
-        console.error('Error fetching gifts:', e);
-        res.status(500).send('Error fetching gifts');
-    }
+// Get all gifts
+router.get('/', async (req, res, next) => {
+	logger.info('/ called');
+	try {
+		const db = await connectToDatabase();
+
+		const collection = db.collection("gifts");
+		const gifts = await collection.find({}).toArray();
+		res.json(gifts);
+	} catch (e) {
+		logger.console.error('oops something went wrong', e)
+		next(e);
+	}
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("gifts");
+// Get a single gift by ID
+router.get('/:id', async (req, res, next) => {
+	try {
+		const db = await connectToDatabase();
+		const collection = db.collection("gifts");
+		const id = req.params.id;
+		const gift = await collection.findOne({ id: id });
 
-        const id = req.params.id;
-        const gift = await collection.findOne({ id: id });
+		if (!gift) {
+			return res.status(404).send("Gift not found");
+		}
 
-        if (!gift) {
-            return res.status(404).send('Gift not found');
-        }
-
-        res.json(gift);
-    } catch (e) {
-        console.error('Error fetching gift:', e);
-        res.status(500).send('Error fetching gift');
-    }
+		res.json(gift);
+	} catch (e) {
+		next(e);
+	}
 });
+
 
 // Add a new gift
 router.post('/', async (req, res, next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("gifts");
-        const gift = await collection.insertOne(req.body);
+	try {
+		const db = await connectToDatabase();
+		const collection = db.collection("gifts");
+		const gift = await collection.insertOne(req.body);
 
-        res.status(201).json(gift.ops[0]);
-    } catch (e) {
-        next(e);
-    }
+		res.status(201).json(gift.ops[0]);
+	} catch (e) {
+		next(e);
+	}
 });
 
 module.exports = router;
