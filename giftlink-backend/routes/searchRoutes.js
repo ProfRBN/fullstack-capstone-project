5eca2d79848776d3298a15d83ba67ad936fe35ea
ProfRBN <nylfson@yahoo.com.br>
2026-04-09 14:15:12 -0400

    Fix lint errors in searchRoutes

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,23 +1,18 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
 
-// Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-
-        // Task 1: Connect to MongoDB
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
-        // Initialize the query object
         let query = {};
 
-        // Task 2: check if the name exists and is not empty
         if (req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" };
         }
 
-        // Task 3: Add other filters to the query
         if (req.query.category) {
             query.category = req.query.category;
         }
@@ -28,7 +23,6 @@ router.get('/', async (req, res, next) => {
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts
         const gifts = await collection.find(query).toArray();
         res.json(gifts);
     } catch (e) {
